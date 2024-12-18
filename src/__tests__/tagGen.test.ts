f3b940c295abf684e3f2ca8835fa0fc3fc1fc700
Iaroslav Rusanov <sco.nuff@gmail.com>
2024-12-18 15:17:03 +0300

    fix: delete vite-project + lint

--- src/__tests__/tagGen.test.ts
+++ src/__tests__/tagGen.test.ts
@@ -1,18 +1,18 @@
-import { expect, test } from "vitest";
+import { expect, test } from 'vitest';
 import TagGen from '../tagGen';
 
 test('test toString() tag', () => {
-  const tagSingle = new TagGen("input", { type: "submit", value: "Save" });
+  const tagSingle = new TagGen('input', { type: 'submit', value: 'Save' });
   const testResultSingle = '<input type="submit" value="Save">';
 
   expect(tagSingle.toString()).toEqual(testResultSingle);
 
-  const tagDouble = new TagGen("label", { for: "email" }, "Email");
+  const tagDouble = new TagGen('label', { for: 'email' }, 'Email');
   const testResultDoble = '<label for="email">Email</label>';
 
   expect(tagDouble.toString()).toEqual(testResultDoble);
 
-  const tagOnly = new TagGen("div");
+  const tagOnly = new TagGen('div');
   const testResultTagOnly = '<div></div>';
 
   expect(tagOnly.toString()).toEqual(testResultTagOnly);
